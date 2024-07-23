818afa9a736df7a6a3e21224351cdcb263f7059a
Ebdollah <l201165@lhr.nu.edu.pk>
2024-07-23 15:50:59 +0500

    added basic project

---
+++ vue-adv/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    {{ msg }} 
+  </h1>
+</template>
+<script>
+export default{
+  name: 'HelloWorld',
+  props: {
+  msg: String
+}
+}
+</script>
